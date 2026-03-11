3197a69215e913aa1710b9afd5b8acefcdf8af79
TimerErTim <timerertim@gmail.com>
2026-03-11 23:05:55 +0100

    build: remove self url

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,4 @@
 export const config = {
-  selfBaseUrl: import.meta.env.VITE_PUBLIC_URL ?? "https://plapok.timerertim.eu",
   spacetimeDbUri: import.meta.env.VITE_SPACETIMEDB_URI ?? "https://maincloud.spacetimedb.com",
   spacetimeDbModule: import.meta.env.VITE_SPACETIMEDB_MODULE ?? "timerertim-plapok-01",
 };
